acde6ba44263efc19eab8c9d1830bceaf45e6e75
Jacky Wu <jacky.wu@heidi.cloud>
2026-08-31 22:19:27 -0700

    Refactor Header component and update layout styles
    - Restructured the Header component to group text elements into a single column for improved layout.
    - Enhanced styling for the main body and header to ensure better alignment and spacing on larger screens.
    - Updated SCSS styles to adjust column behavior and padding, ensuring a cohesive design across different breakpoints.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,16 @@
 export default function Header() {
   return (
     <header id="header">
-      <h2 style={{ fontFamily: "Clicker Script" }}>In Loving Memory</h2>
-      <h1 style={{ fontFamily: "Monsieur La Doulaise" }}>Haijie Liu</h1>
-      <h3 style={{ fontFamily: "Clicker Script" }}>1973 - 2020</h3>
-      <br />
       <div className="row">
+        <div className="column">
+          <h2 style={{ fontFamily: "Clicker Script" }}>In Loving Memory</h2>
+          <h1 style={{ fontFamily: "Monsieur La Doulaise" }}>Haijie Liu</h1>
+          <h3 style={{ fontFamily: "Clicker Script" }}>1973 - 2020</h3>
+        </div>
         <div className="column" style={{ fontFamily: "League Script" }}>
           <p>You have fought the good fight,</p>
-          <p style={{ fontFamily: "League Script" }}>
-            You have finished the race,
-          </p>
-          <p style={{ fontFamily: "League Script" }}>
-            You have kept the faith.
-          </p>
+          <p>You have finished the race,</p>
+          <p>You have kept the faith.</p>
         </div>
       </div>
     </header>
